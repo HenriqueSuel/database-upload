92950e25617ceaa6bb2707c8435fd06af5bfb11d
Henrique <h.suel17@hotmail.com>
2020-05-11 14:54:39 -0300

    teste finalizado

---
+++ src/config/upload.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
+
+const tmpFoltder = path.resolve(__dirname, '..', '..', 'tmp');
+
+export default {
+  directory: tmpFoltder,
+  storage: multer.diskStorage({
+    destination: tmpFoltder,
+    filename: (request, file, callback) => {
+      const fileHash = crypto.randomBytes(10).toString('HEX');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,23 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const balacence = transactions.reduce(
+      (previous: Balance, current: Transaction) => {
+        if (current.type === 'income') {
+          // eslint-disable-next-line no-param-reassign
+          previous.income += Number(current.value);
+        } else if (current.type === 'outcome') {
+          // eslint-disable-next-line no-param-reassign
+          previous.outcome += Number(current.value);
+        }
+        // eslint-disable-next-line no-param-reassign
+        previous.total = Number(previous.income) - Number(previous.outcome);
+        return previous;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+    return balacence;
   }
 }
 
